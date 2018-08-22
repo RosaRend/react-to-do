f0904d7bc1fb63f68a0c9559d97ad15d57461da2
RosaRend <ice2meetu@outlook.com>
2018-08-22 18:38:56 -0400

    Small changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0
+    // imageUrl: "https://picsum.photos/200"
+  };
+
+  style = {
+    fontSize: 50,
+    fontWeight: "bold"
   };
 
   render() {
+    const classes = ""
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span style={this.style} className="badge badge-warning m-2">
+          {this.formatCount()}
+        </span>
         {/* _____^ You can write any js expression meaning anything that produces a value */}
-        <button>Increment</button>
+        <button type="button" className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </React.Fragment>
     );
   }
