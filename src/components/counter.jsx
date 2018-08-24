0a8e11d8930a755681fec53187b88c5e509f0cf2
RosaRend <ice2meetu@outlook.com>
2018-08-23 23:12:28 -0400

    method for badges

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["task1", "task2", "task3"]
     // imageUrl: "https://picsum.photos/200"
   };
 
@@ -12,20 +13,37 @@ class Counter extends Component {
   };
 
   render() {
-    const classes = ""
+    // let classes = this.getBadgeClasses();
+
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={this.style} className="badge badge-warning m-2">
+        <span style={this.style} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         {/* _____^ You can write any js expression meaning anything that produces a value */}
-        <button type="button" className="btn btn-secondary btn-sm">
+        <button
+          style={{ height: 50 }}
+          type="button"
+          className="btn btn-secondary btn-sm"
+        >
           Increment
         </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    //^append
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
