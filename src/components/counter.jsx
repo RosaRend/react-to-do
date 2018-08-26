e47078ad833afb4538f1be623039ee8e1c4f9eb4
RosaRend <ice2meetu@outlook.com>
2018-08-25 21:08:19 -0400

    render w conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,8 +7,21 @@ class Counter extends Component {
     // imageUrl: "https://picsum.photos/200"
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>No tasks set</p>;
+    //This is how you render tags conditionally
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+        {/* Your now rendering a list of items in react */}
+      </ul>
+    );
+  }
+
   style = {
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: "bold"
   };
 
@@ -18,36 +31,33 @@ class Counter extends Component {
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={this.style} className={this.getBadgeClasses()}>
+        {/* <span style={this.style} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         {/* _____^ You can write any js expression meaning anything that produces a value */}
-        <button
-          style={{ height: 50 }}
+        {/* <button
+          style={{ height: 30 }}
           type="button"
           className="btn btn-secondary btn-sm"
         >
           Increment
-        </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        </button>{" "} 
+        */}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    //^append
-    return classes;
-  }
+  // getBadgeClasses() {
+  //   let classes = "badge m-2 badge-";
+  //   classes += this.state.count === 0 ? "warning" : "primary";
+  //   //^append
+  //   return classes;
+  // }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+  // formatCount() {
+  //   const { count } = this.state;
+  //   return count === 0 ? "Zero" : count;
+  // }
 }
 
 export default Counter;
